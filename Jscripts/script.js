b012f04d9003c9bd892e7741bda483b57242bd83
Itumeleng <seralakagiso88gmail.com>
2022-06-08 13:38:29 +0200

    I have added a function called operate to the script.js file and this function randomly calls one of the four basic operations functions on the the numbers passed in to it

--- Jscripts/script.js
+++ Jscripts/script.js
@@ -13,3 +13,23 @@
  function divide(a, b) {
      return a/b;
  }
+
+ function random() {
+     return Math.floor(Math.random()*4);
+}
+
+ function operate(a, b) {
+     const randomNumber = random();
+     
+     switch(randomNumber) {
+         case 0:
+             return add(a, b);
+         case 1:
+             return subtract(a, b);
+         case 2:
+             return multiply(a, b);
+         case 3:
+             return divide(a, b);      
+
+     }    
+ }
