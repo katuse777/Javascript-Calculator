059f330a8b5b7f6ae91604f69e65c86806cc532a
Itumeleng <seralakagiso88gmail.com>
2022-06-08 15:13:54 +0200

    I've added a variable that stored a reference to a button on the dom for every button

--- Jscripts/script.js
+++ Jscripts/script.js
@@ -33,3 +33,23 @@
 
      }    
  }
+
+ const display = document.getElementById('screen');
+ const onButton = document.getElementById('on');
+ const button0 = document.getElementById('0');
+ const button1 = document.getElementById('1');
+ const button2 = document.getElementById('2');
+ const button3 = document.getElementById('3');
+ const button4 = document.getElementById('4');
+ const button5 = document.getElementById('5');
+ const button6 = document.getElementById('6');
+ const button7 = document.getElementById('7');
+ const button8 = document.getElementById('8');
+ const button9 = document.getElementById('9');
+ const addButton = document.getElementById('add');
+ const minusButton = document.getElementById('minus');
+ const multiplyButton = document.getElementById('multi');
+ const divideButton = document.getElementById('divide');
+ const clear = document.getElementById('clear');
+ const operateButton = document.getElementById('operate');
+ const deleteButton = document.getElementById('delete');
