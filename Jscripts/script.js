f431d544bdf30b75db7344d7d841f6c39553352d
Itumeleng <seralakagiso88gmail.com>
2022-06-09 11:15:54 +0200

    When a number is inserted into the calculator and an operation button is pressed, that number and operation symbol get displayed at the top of the screen

--- Jscripts/script.js
+++ Jscripts/script.js
@@ -1,83 +1,79 @@
- function add(a, b) {
-     return a + b;
- }
-
- function subtract(a, b) {
-     return a - b;
- }
-
- function multiply(a, b) {
-     return a*b;
- }
-
- function divide(a, b) {
-     return a/b;
- }
-
- function random() {
-     return Math.floor(Math.random()*4);
+function add(a, b) {
+    return a + b;
 }
 
- function operate(a, b) {
-     const randomNumber = random();
-     
-     switch(randomNumber) {
-         case 0:
-             return add(a, b);
-         case 1:
-             return subtract(a, b);
-         case 2:
-             return multiply(a, b);
-         case 3:
-             return divide(a, b);      
+function subtract(a, b) {
+    return a - b;
+}
 
-     }    
- }
+function multiply(a, b) {
+    return a * b;
+}
 
- const display = document.getElementById('screen');
- const onButton = document.getElementById('on');
- const button0 = document.getElementById('0');
- const button1 = document.getElementById('1');
- const button2 = document.getElementById('2');
- const button3 = document.getElementById('3');
- const button4 = document.getElementById('4');
- const button5 = document.getElementById('5');
- const button6 = document.getElementById('6');
- const button7 = document.getElementById('7');
- const button8 = document.getElementById('8');
- const button9 = document.getElementById('9');
- const addButton = document.getElementById('add');
- const minusButton = document.getElementById('minus');
- const multiplyButton = document.getElementById('multi');
- const divideButton = document.getElementById('divide');
- const clear = document.getElementById('clear');
- const operateButton = document.getElementById('operate');
- const deleteButton = document.getElementById('delete');
- const setUp = document.getElementById('set-up');
- const answerDisplay = document.getElementById('answer');
+function divide(a, b) {
+    return a / b;
+}
 
- onButton.addEventListener('click', () => {
-     answerDisplay.textContent = 0;
- })
+function random() {
+    return Math.floor(Math.random() * 4);
+}
 
- function onScreen(num) {
-     answerDisplay.textContent += num;
-     setUp.textContent += num;
-     const firstValue = Number(setUp.textContent);
-     console.log(firstValue);
-     console.log(typeof firstValue);
- }
+function operate(a, b) {
+    const randomNumber = random();
 
- button0.addEventListener('click', () => {
-     onScreen(0);
- });
- button1.addEventListener('click', () => {
-     onScreen(1);
- });
- button2.addEventListener('click', () => {
-     onScreen(2);
- });
- button3.addEventListener('click', () => {
+    switch (randomNumber) {
+        case 0:
+            return add(a, b);
+        case 1:
+            return subtract(a, b);
+        case 2:
+            return multiply(a, b);
+        case 3:
+            return divide(a, b);
+
+    }
+}
+
+const display = document.getElementById('screen');
+const onButton = document.getElementById('on');
+const button0 = document.getElementById('0');
+const button1 = document.getElementById('1');
+const button2 = document.getElementById('2');
+const button3 = document.getElementById('3');
+const button4 = document.getElementById('4');
+const button5 = document.getElementById('5');
+const button6 = document.getElementById('6');
+const button7 = document.getElementById('7');
+const button8 = document.getElementById('8');
+const button9 = document.getElementById('9');
+const addButton = document.getElementById('add');
+const minusButton = document.getElementById('minus');
+const multiplyButton = document.getElementById('multi');
+const divideButton = document.getElementById('divide');
+const clear = document.getElementById('clear');
+const operateButton = document.getElementById('operate');
+const deleteButton = document.getElementById('delete');
+const setUp = document.getElementById('set-up');
+const answerDisplay = document.getElementById('answer');
+
+onButton.addEventListener('click', () => {
+    answerDisplay.textContent = '';
+})
+
+function onScreen(num) {
+    answerDisplay.textContent += num;
+}
+
+button0.addEventListener('click', () => {
+    onScreen(0);
+});
+button1.addEventListener('click', () => {
+    onScreen(1);
+});
+button2.addEventListener('click', () => {
+    onScreen(2);
+});
+button3.addEventListener('click', () => {
     onScreen(3);
 });
 button4.addEventListener('click', () => {
@@ -95,6 +91,36 @@ button7.addEventListener('click', () => {
 button8.addEventListener('click', () => {
     onScreen(8);
 });
- button9.addEventListener('click', () => {
-     onScreen(9);
- });
+button9.addEventListener('click', () => {
+    onScreen(9);
+});
+
+
+addButton.addEventListener('click', () => {
+    const firstValue = Number(answerDisplay.textContent);
+    setUp.textContent += firstValue;
+    setUp.textContent += ' + ';
+    answerDisplay.textContent = '';
+
+})
+minusButton.addEventListener('click', () => {
+    const firstValue = Number(answerDisplay.textContent);
+    setUp.textContent += firstValue;
+    setUp.textContent += ' - ';
+    answerDisplay.textContent = '';
+
+})
+multiplyButton.addEventListener('click', () => {
+    const firstValue = Number(answerDisplay.textContent);
+    setUp.textContent += firstValue;
+    setUp.textContent += ' x ';
+    answerDisplay.textContent = '';
+
+})
+divideButton.addEventListener('click', () => {
+    const firstValue = Number(answerDisplay.textContent);
+    setUp.textContent += firstValue;
+    setUp.textContent += '/';
+    answerDisplay.textContent = '';
+
+})
